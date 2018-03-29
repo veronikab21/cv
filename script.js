b6bb469e630ba0c82b98db8aed2eb86fd6ad6086
Veronika Bartosova <veronika.bartosova@synapse.com>
2018-03-29 14:10:51 +0200

    added media queries

--- script.js
+++ script.js
@@ -22,3 +22,14 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+let nav = document.querySelector('nav');
+let navicon = document.querySelector(".ion-navicon");
+
+navicon.addEventListener('click', function(){
+  if (nav.style.display === 'block'){
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'block';
+  }
+})
